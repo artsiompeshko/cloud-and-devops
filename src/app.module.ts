4c720135e69147d23d68c22f8a43b856cb52467a
Artsiom <artsiom_peshko@epam.com>
2021-04-14 14:20:32 +0300

    add client serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { join } from 'path';
+
+console.log(__dirname);
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'static_assets'),
+    }),
+  ],
 })
 export class AppModule {}
